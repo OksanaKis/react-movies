e71a862f7a102256c974b49badd2c3005fe410bf
OksanaKis <oksanakisetoja@gmail.com>
2025-02-17 14:49:14 -0800

    Component
    Create a component MovieCard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css'
+import './App.css';
+import MovieCard from './components/MovieCard';
 
 function App() {
 
   return (
     <>
-      <div>Hello</div>
+      <MovieCard movie={{title: "Tim's Film", release_date: "2024"}}/>
     </>
   )
 }
